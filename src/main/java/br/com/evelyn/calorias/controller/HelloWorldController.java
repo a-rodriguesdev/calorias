54328a43c792a2ca34177b5159939da0e7d4b2a1
Évelyn Rodrigues <rodrigueseconsultoria@gmail.com>
2026-05-07 17:10:12 -0300

    Add dependencies for JPA, Flyway, and validation; implement HelloWorldController with new endpoints; enhance Usuario model with JPA annotations and fields.

--- src/main/java/br/com/evelyn/calorias/controller/HelloWorldController.java
+++ src/main/java/br/com/evelyn/calorias/controller/HelloWorldController.java
@@ -1,4 +1,20 @@
 package br.com.evelyn.calorias.controller;
 
-public class HelloWorldController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {    // ENDPOINT
+
+    @GetMapping("/hello")
+    public String getHelloWorld(){
+        return "<h1>Hello World!<h1>";
+    }
+
+    @GetMapping("/ola")
+    public String getOlaMundo(){
+        return "<h1>Olá Mundo!<h1>";
+    }
 }
--- src/main/java/br/com/evelyn/calorias/model/Usuario.java
+++ src/main/java/br/com/evelyn/calorias/model/Usuario.java
@@ -1,4 +1,31 @@
 package br.com.evelyn.calorias.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Usuario {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "SEQ_USUARIOS"
+    )
+    @SequenceGenerator(
+            name = "SEQ_USUARIOS",
+            sequenceName = "SEQ_USUARIOS",
+            allocationSize = 1
+    )
+    @Column(name = "usuario_id")
+    private Long usuarioId;
+
+    private String nome;
+    private String email;
+    private String senha;
 }
