1ea383dcda324bce05b30dbd17918f6ddfb659fe
Évelyn Rodrigues <rodrigueseconsultoria@gmail.com>
2026-05-07 18:10:05 -0300

    Refactor UsuarioRepository to extend JpaRepository and implement UsuarioService methods for user management, including save, find by ID, list all, update, and delete functionalities.

--- src/main/java/br/com/evelyn/calorias/repository/UsuarioRepository.java
+++ src/main/java/br/com/evelyn/calorias/repository/UsuarioRepository.java
@@ -1,4 +1,7 @@
 package br.com.evelyn.calorias.repository;
 
-public interface UsuarioRepository {
+import br.com.evelyn.calorias.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
